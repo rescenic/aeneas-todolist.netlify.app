e5f55c84f002c8f6e0b5cd876be3cbc6fb7bf24e
Muhammad Ridwan Hakim <adminrescen@gmail.com>
2023-02-04 17:47:40 +0700

    v.1.1

--- clock.js
+++ clock.js
@@ -9,7 +9,7 @@ function updateClock() {
   document.getElementById("clock").innerHTML = `${hours}:${minutes}:${seconds}`;
 }
 
-function countdown() {
+function countDown() {
   const countDownDate = new Date("Jan 1, 2024 00:00:00").getTime();
   const now = new Date().getTime();
   const distance = countDownDate - now;
@@ -20,5 +20,17 @@ function countdown() {
   document.getElementById("countdown").innerHTML = `${days}hari ${hours}jam ${minutes}menit ${seconds}detik`;
 }
 
+function countUp() {
+  const startDate = new Date("May 31, 2022 21:00:00").getTime();
+  const now = new Date().getTime();
+  const distance = now - startDate;
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("countup").innerHTML = `${days}hari ${hours}jam ${minutes}menit ${seconds}detik`;
+}
+
 setInterval(updateClock, 1000);
-setInterval(countdown, 1000);
+setInterval(countDown, 1000);
+setInterval(countUp, 1000);
